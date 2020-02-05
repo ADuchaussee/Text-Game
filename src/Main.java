d353c5d82678523b40d4aec6b1607c1dfb6a6645
unknown <andrewduchaussee@gmail.com>
2020-02-05 15:14:26 -0500

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println ("Welcome to Elden Gate");
+		Scanner In = new Scanner(System.in);
+		String Input = In.nextLine();
+}
+	
+	public static void Introduction()
+	{
+		
+	}
+	
+
+}
